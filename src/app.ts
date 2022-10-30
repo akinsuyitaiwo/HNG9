fa09fa255d23163a0b9e7da565fc0cc0c2c9d736
akinsuyitaiwo <teeboidayo@gmail.com>
2022-10-30 17:25:41 +0100

    task update

--- src/app.ts
+++ src/app.ts
@@ -5,10 +5,9 @@ import { errorResponse, handleError } from "./utils/responses"
 
 const app = express()
 const port = 3000;
-app.use(cors())
+
+app.use(cors());
 app.use(express.json());
-
-
 
 app.get("/", (req, res) => {
     try {
@@ -18,10 +17,10 @@ app.get("/", (req, res) => {
             age: 25,
             bio: `I am a backend developer(nodejs). Asides coding i enjoy playing football and video games. I'm looking forward to getting better with being a developer.`
         }
-        return res.json(details)
+        return res.json(details);
     } catch (error) {
-        handleError(error, req)
-        return errorResponse
+        handleError(error, req);
+        return errorResponse(res, 500, "server error")
     }
 });
 
