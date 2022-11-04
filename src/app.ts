5989b176a03dc0f2ada10c48e71b2f23f7143e15
akinsuyitaiwo <teeboidayo@gmail.com>
2022-11-04 20:29:43 +0100

    task 2

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,6 @@
 import express from "express";
 import cors from "cors";
-import { formatInterface } from "./utils/interface"
+import { formatInterface, postInterface, PostAnswers, operation } from "./utils/interface"
 import { errorResponse, handleError } from "./utils/responses"
 
 const app = express()
@@ -22,6 +22,40 @@ app.get("/", (req, res) => {
         handleError(error, req);
         return errorResponse(res, 500, "server error")
     }
+});
+
+app.post("/",(req, res) => {
+    try{
+        const{x,y, operation_type}:postInterface = req.body;
+        if(!operation_type || !x || !y){
+            return errorResponse( res, 400 , "you have input an invalid input")
+        }
+        const X = Number(x);
+        const Y = Number(y);
+    let answer;
+    if(operation_type === "addition"){
+        answer = X + Y;
+    }else if(operation_type === "multiplication"){
+        answer = X * Y;
+    }else if(operation_type === "subtraction"){
+        answer = X - Y;
+    }else{
+        return errorResponse(res, 400, "Invalid operation" )
+    }
+    const outcome : PostAnswers ={
+        slackUsername:"akinsuyitaiwo",
+        operation_type,
+        answer
+    };
+    return res.json(outcome)
+    }catch (error){
+        return errorResponse(res, 500, "serve error");
+    }
+    app.use((req, res) => res.status(404).send({
+        status: "error",
+        error: "Not found",
+        message: "Incorrect route .",
+      }));
 });
 
 app.listen(3000, () => {
--- src/utils/interface.ts
+++ src/utils/interface.ts
@@ -3,4 +3,20 @@ export interface formatInterface{
     backend : boolean;
     age? : number;
     bio? : string;
+} 
+
+export enum operation{
+    addition = "addition",
+    multiplication = "multiplication",
+    subtraction = "subtraction"
+}
+export interface postInterface{
+    operation_type : operation;
+    x: number,
+    y: number
+}
+export interface PostAnswers{
+    slackUsername?: string;
+    operation_type : operation;
+    answer? : number
 }
